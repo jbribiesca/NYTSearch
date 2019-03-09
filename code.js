b3ebc80b6d905aca6e3fae5e832da6feb4a0da7a
jbribiesca <bribiesca@gmail.com>
2019-03-09 13:50:25 -0600

    fixed date

--- code.js
+++ code.js
@@ -2,8 +2,8 @@
 
 $("button").on("click", function() {
     var query = $("#searchterm").val();
-    var begin_date = "&begin_date=" + $("#begindate").val();
-    var end_date = "&end_date=" +  + $("#enddate").val();
+    var begin_date = "&begin_date=20180101" + $("#begindate").val();
+    var end_date = "&end_date=20190101" +  + $("#enddate").val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
       query + "&api-key=Wg0SfSHkxKoSMxEpmAwGAg0vDhaQu9qg" + begin_date + end_date;
 
